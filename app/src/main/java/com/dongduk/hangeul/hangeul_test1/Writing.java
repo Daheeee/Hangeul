fea70e51dbc6197c1c644badac341bc52fef7341
jiyoung111 <jiyoung928@gmail.com>
2017-09-25 23:48:28 +0900

    update writing

--- app/src/main/java/com/dongduk/hangeul/hangeul_test1/Writing.java
+++ app/src/main/java/com/dongduk/hangeul/hangeul_test1/Writing.java
@@ -11,7 +11,7 @@ public class Writing {
     private long rid;
     private long wid;
     private String uid;
-    private Date date;
+    private String date;
     private String writing;
 
 
@@ -39,11 +39,11 @@ public class Writing {
         this.uid = uid;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
 
